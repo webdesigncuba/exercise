c74d91cbf253e71cb1098eceedeaab70c6c4c06d
David Cordero Rosales <webdesigncuba@gmai.com>
2023-09-13 10:42:20 -0300

    Added Controller and Contact Model.

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Contact::all();
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'contact' => 'required|max:255',
+            'email' => 'required|max:255',
+          ]);
+        Contact::create($request->all());
+          return redirect()->route('posts.index')
+            ->with('success','Post created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
